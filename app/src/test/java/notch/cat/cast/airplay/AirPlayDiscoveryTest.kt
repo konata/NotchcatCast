3c20f691fec01683e542d68925c3f02cd4a0ddb7
konata <beriru@qq.com>
2026-06-27 15:02:05 +0800

    fix: advertise only supported AirPlay video codec

--- app/src/test/java/notch/cat/cast/airplay/AirPlayDiscoveryTest.kt
+++ app/src/test/java/notch/cat/cast/airplay/AirPlayDiscoveryTest.kt
@@ -11,12 +11,12 @@ class AirPlayDiscoveryTest {
     val raop = AirPlayDiscovery.raopTxt("pk-value")
 
     assertEquals("62:6D:04:15:E7:2F", id)
-    assertEquals("0x5A7FFEE6,0x400", airplay["features"])
+    assertEquals("0x5A7FFEE6,0x0", airplay["features"])
     assertEquals("0x4", airplay["flags"])
     assertEquals("AppleTV3,2", airplay["model"])
     assertEquals("uuid-value", airplay["pi"])
     assertEquals("pk-value", airplay["pk"])
-    assertEquals("0x5A7FFEE6,0x400", raop["ft"])
+    assertEquals("0x5A7FFEE6,0x0", raop["ft"])
     assertEquals("0x4", raop["sf"])
     assertEquals("AppleTV3,2", raop["am"])
   }
